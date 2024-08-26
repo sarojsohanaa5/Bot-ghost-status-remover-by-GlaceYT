7745d47dc6743864e66c61a5206dbd13949cff7c
sarojsohanaa5 <112052380+sarojsohanaa5@users.noreply.github.com>
2024-08-26 09:15:41 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Paying All ","Inviters✨"];
 
 
 let currentIndex = 0;
